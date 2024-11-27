24e4d9b7819f5ef1ed6ce75eb5a2abed9f5f2631
Janek7 <34423753+Janek7@users.noreply.github.com>
2024-11-27 21:39:39 +0000

    Deploying to gh-pages from @ Janek7/recipes@314956ecb556e3abc78faa9c3df2c49924b5eec4 🚀

---
+++ js/proposal.js
@@ -0,0 +1,34 @@
+const recipeData = [
+    {"slug": "brokoli-suppe", "title": "Brokoli Suppe", "category": "Suppe"},
+    {"slug": "reisauflauf", "title": "Reisauflauf", "category": "Auflauf"},
+    {"slug": "spinatlasagne", "title": "Spinatlasagne", "category": "Auflauf"}
+];
+
+
+function proposeRandomRecipe() {
+    var recipeName = selectRandomRecipe();
+    editSearchResultElement(recipeName);
+}
+
+
+function selectRandomRecipe() {
+
+    var recipeTitles = recipeData.map(recipe => recipe.title);
+    let randomRecipe = recipeTitles[Math.floor(Math.random() * recipeTitles.length)];
+    console.log("Randomly selected recipe: " + randomRecipe);
+    return randomRecipe;
+
+}
+
+
+function editSearchResultElement(recipeName) {
+    
+    // 1) toggle search result box visibilty
+    var resultBox = document.getElementById("proposal-result-box")
+    resultBox.style.display = "block"; // Show the element
+
+    // 2) Edit header
+    var titleHeader = document.getElementById("proposal-title")
+    titleHeader.innerText = recipeName;
+
+}
